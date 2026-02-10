41edd8ce18dcfa1f744e018bf635c30091150e41
Christopher Tan <164590127+christophertna@users.noreply.github.com>
2026-02-10 17:45:10 -0500

    Add placeholder classes

--- Player.h
+++ Player.h
@@ -5,21 +5,55 @@
 #include <vector>
 #include <iostream>
 
-// Forward declarations 
-class Territory;
-class Hand;
-class OrderList;  
-class Order;      
+// For Assignment 1, using placeholder classes
+// Unfinished classes for compilation only
 
+class Territory {
+public:
+    Territory(std::string name) : name(name) {}
+    std::string getName() const { return name; }
+private:
+    std::string name;
+};
+
+class Hand {
+public:
+    Hand() {}
+    friend std::ostream& operator<<(std::ostream& out, const Hand& h) {
+        out << "[Hand placeholder]";
+        return out;
+    }
+};
+
+class OrderList {
+public:
+    OrderList() {}
+    void add(class Order* order) { /* placeholder */ }
+    friend std::ostream& operator<<(std::ostream& out, const OrderList& ol) {
+        out << "[OrderList placeholder]";
+        return out;
+    }
+};
+
+class Order {
+public:
+    Order() {}
+};
+
+
+// actual Player class
 class Player {
 public:
     Player(std::string name);
     Player(const Player& other);
     Player& operator=(const Player& other);
-    
     friend std::ostream& operator<<(std::ostream& out, const Player& p);
-    
     ~Player();
+
+    // Defend/Attack and issue order methods
+    std::vector<Territory*>* toDefend();
+    std::vector<Territory*>* toAttack();
+    void issueOrder();
 
     std::string* getName() const;
     std::vector<Territory*>* getTerritories() const;
@@ -27,11 +61,6 @@ public:
     OrderList* getOrders() const;
 
     void addTerritory(Territory* t);
-    
-    // Required methods
-    std::vector<Territory*>* toDefend();
-    std::vector<Territory*>* toAttack();
-    void issueOrder();
 
 private:
     std::string* name;
